11a59bf08cc09584dc62b529dc04b001b9ede80d
Michael Harmon <mfh.scoobydoo@gmail.com>
2025-02-13 20:55:08 -0700

    check ribbon width and allow for swapping ribbon UV direction

--- utils/ribbon.py
+++ utils/ribbon.py
@@ -5,6 +5,7 @@ def generate_ribbon (
         nurbs_surface_name: str, 
         number_of_joints: int = 8,
         cyclic: bool = False,
+        swap_uv: bool = False,
         local_space: bool = False, 
         control_joints: bool = False, 
         number_of_controls: int = None,
@@ -13,7 +14,12 @@ def generate_ribbon (
     ribbon_length: float = 2
     surface_shape = cmds.listRelatives(nurbs_surface_name, shapes=True)[0]
     if cmds.nodeType(surface_shape) == "nurbsSurface":
-        ribbon_length = cmds.getAttr(str(surface_shape)+".minMaxRangeU")[0][1]
+        if swap_uv:
+            ribbon_length = cmds.getAttr(str(surface_shape)+".minMaxRangeV")[0][1]
+            ribbon_width = cmds.getAttr(str(surface_shape)+".minMaxRangeU")[0][1]
+        else:
+            ribbon_length = cmds.getAttr(str(surface_shape)+".minMaxRangeU")[0][1]
+            ribbon_width = cmds.getAttr(str(surface_shape)+".minMaxRangeV")[0][1]
         if not number_of_controls:
             number_of_controls = int(ribbon_length)
             if half_controls:
@@ -27,20 +33,37 @@ def generate_ribbon (
             if cyclic:
                 for i in range(number_of_controls):
                     control_spacing: float = (ribbon_length/(number_of_controls))
-                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=(control_spacing*i), parameterV=0.5)
+                    u: float = (control_spacing*i)
+                    v: float = ribbon_width/2
+                    if swap_uv:
+                        position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=v, parameterV=u)
+                        print(u,v)
+                    else:
+                        position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=u, parameterV=v)
+                        print(u,v)
                     cmds.select(ribbon_group, replace=True)
                     joint_name = cmds.joint(position=position, radius=1, name=str(ribbon_object)+str(i+1)+"_ControlJoint")
             else:
                 for i in range(number_of_controls + 1):
                     control_spacing: float = (ribbon_length/(number_of_controls))
-                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=(control_spacing*i), parameterV=0.5)
+                    u: float = (control_spacing*i)
+                    v: float = ribbon_width/2
+                    if swap_uv:
+                        position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=v, parameterV=u)
+                    else:
+                        position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=u, parameterV=v)
                     cmds.select(ribbon_group, replace=True)
                     joint_name = cmds.joint(position=position, radius=1, name=str(ribbon_object)+str(i+1)+"_ControlJoint")
 
         if cyclic:
             for i in range(number_of_joints):
                 cmds.select(ribbon_group, replace=True)
-                position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=((i/(number_of_joints))*ribbon_length), parameterV=0.5)
+                u: float = ((i/(number_of_joints))*ribbon_length)
+                v: float = ribbon_width/2
+                if swap_uv:
+                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=v, parameterV=u)
+                else:
+                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=u, parameterV=v)
                 joint_name = cmds.joint(position=position, radius=0.5, name=str(ribbon_object)+"_point"+str(i+1)+"_DEF")
                 cmds.select(ribbon_object+".uv"+"["+str((i/(number_of_joints-2))*2)+"]"+"[0.5]", replace=True)
                 cmds.select(joint_name, add=True)
@@ -50,9 +73,17 @@ def generate_ribbon (
         else:
             for i in range(number_of_joints):
                 cmds.select(ribbon_group, replace=True)
-                position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=((i/(number_of_joints-1))*ribbon_length), parameterV=0.5)
+                u: float = ((i/(number_of_joints-1))*ribbon_length)
+                v: float = ribbon_width/2
+                if swap_uv:
+                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=v, parameterV=u)
+                else:
+                    position = cmds.pointOnSurface(ribbon_object, position=True, parameterU=u, parameterV=v)
                 joint_name = cmds.joint(position=position, radius=0.5, name=str(ribbon_object)+"_point"+str(i+1)+"_DEF")
-                cmds.select(ribbon_object+".uv"+"["+str((i/(number_of_joints-1))*2)+"]"+"[0.5]", replace=True)
+                if swap_uv:
+                    cmds.select(ribbon_object+".uv"+"["+str(v)+"]"+"["+str(u)+"]", replace=True)
+                else:
+                    cmds.select(ribbon_object+".uv"+"["+str(u)+"]"+"["+str(v)+"]", replace=True)
                 cmds.select(joint_name, add=True)
                 cmds.UVPin()
                 if not local_space:
