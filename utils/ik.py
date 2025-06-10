d2ade360450200b35bb6bc7699919f9adc0820e3
Michael Harmon <mfh.scoobydoo@gmail.com>
2025-06-10 12:57:38 -0600

    IK Blend Improvements

--- utils/ik.py
+++ utils/ik.py
@@ -8,23 +8,23 @@ def ik_fk_blend(ik_joint: str, fk_joint: str, blended_joint:str, blend_attr: str
         blended_joint: The joint to give the blended transform
         blend_attr: Attribute to use to determine blending.
     """
-    translate_blend: str = cmds.createNode("blendColors", name=f"{blended_joint}_BlendTranslate")
-    cmds.connectAttr(blend_attr, f"{translate_blend}.blender")
-    cmds.connectAttr(f"{ik_joint}.translate", f"{translate_blend}.color2")
-    cmds.connectAttr(f"{fk_joint}.translate", f"{translate_blend}.color1")
-    cmds.connectAttr(f"{translate_blend}.output", f"{blended_joint}.translate")
+    # Create Blend Matrix node and connect it
+    blend_matrix: str = cmds.createNode("blendMatrix", name=f"{blended_joint}_BlendMatrix")
+    cmds.connectAttr(f"{ik_joint}.matrix", f"{blend_matrix}.inputMatrix")
+    cmds.connectAttr(f"{fk_joint}.matrix", f"{blend_matrix}.target[0].targetMatrix")
+    cmds.connectAttr(blend_attr, f"{blend_matrix}.target[0].weight")
 
-    rotation_blend: str = cmds.createNode("blendColors", name=f"{blended_joint}_BlendRotation")
-    cmds.connectAttr(blend_attr, f"{rotation_blend}.blender")
-    cmds.connectAttr(f"{ik_joint}.rotate", f"{rotation_blend}.color2")
-    cmds.connectAttr(f"{fk_joint}.rotate", f"{rotation_blend}.color1")
-    cmds.connectAttr(f"{rotation_blend}.output", f"{blended_joint}.rotate")
+    # Create the Decomposed Matrix and connect its input
+    decompose_matrix: str = cmds.createNode("decomposeMatrix", name=f"{blended_joint}_BlendMatrixDecompose")
+    cmds.connectAttr(f"{blend_matrix}.outputMatrix", f"{decompose_matrix}.inputMatrix")
 
-    scale_blend: str = cmds.createNode("blendColors", name=f"{blended_joint}_BlendScale")
-    cmds.connectAttr(blend_attr, f"{scale_blend}.blender")
-    cmds.connectAttr(f"{ik_joint}.scale", f"{scale_blend}.color2")
-    cmds.connectAttr(f"{fk_joint}.scale", f"{scale_blend}.color1")
-    cmds.connectAttr(f"{scale_blend}.output", f"{blended_joint}.scale")
+    # Reset blended joint orient and connect the joint to the decomposeMatrix values
+    cmds.setAttr(f"{blended_joint}.jointOrient", 0,0,0, type="float3")
+
+    cmds.connectAttr(f"{decompose_matrix}.outputRotate", f"{blended_joint}.rotate")
+    cmds.connectAttr(f"{decompose_matrix}.outputTranslate", f"{blended_joint}.translate")
+    cmds.connectAttr(f"{decompose_matrix}.outputScale", f"{blended_joint}.scale")
+    cmds.connectAttr(f"{decompose_matrix}.outputShear", f"{blended_joint}.shear")
 
 def ik_fk_blend_list(ik_joints: list[str], fk_joints: list[str], blended_joints: list[str], blend_attr: str) -> None:
     """
