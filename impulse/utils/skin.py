2c2552fc380097cb03b9d8e6e3698dd3257e4269
Michael Harmon <mfh.scoobydoo@gmail.com>
2025-07-11 16:48:43 -0600

    add skinning and fix reverse ik (for real this time)

--- impulse/utils/skin.py
+++ impulse/utils/skin.py
@@ -1,7 +1,9 @@
-from ngSkinTools2 import api as ng
 import maya.cmds as cmds
 from impulse.structs.transform import Vector3 as Vector3
 from impulse.utils import spline as spline
+from ngSkinTools2 import api as ng
+from ngSkinTools2.api.influenceMapping import InfluenceMappingConfig
+from ngSkinTools2.api.transfer import VertexTransferMode
 
 def init_layers(shape: str) -> ng.Layers:
     skin_cluster = ng.target_info.get_related_skin_cluster(shape)
@@ -9,6 +11,26 @@ def init_layers(shape: str) -> ng.Layers:
     base_layer: ng.Layer = layers.add("Base Weights")
     base_layer.set_weights
     return layers
+
+def apply_ng_skin_weights(weights_file: str, geometry: str) -> None:
+    shapes: list[str] = cmds.listRelatives(geometry, children=True, shapes=True) or []
+    if not shapes:
+        raise RuntimeError(f"No shape nodes found on surface: {geometry}")
+    shape: str = shapes[0]
+
+
+    config: InfluenceMappingConfig = InfluenceMappingConfig()
+    config.use_distance_matching = False
+    config.use_name_matching = True
+
+    # run the import
+    ng.import_json(
+    shape,
+    file=weights_file,
+    vertex_transfer_mode=VertexTransferMode.vertexId,
+    influences_mapping_config=config,
+)
+
 
 def get_vertex_positions(shape: str) -> list[Vector3]:
     """
@@ -22,7 +44,7 @@ def get_vertex_positions(shape: str) -> list[Vector3]:
     if surface_type != "mesh":
         raise RuntimeError(f"Unsupported surface type: {surface_type}")
     vertex_count: int = cmds.polyEvaluate(shape, vertex=True)
-    vertex_list = cmds.xform(f"{shape}.vtx[*]", query = True, translation = True, worldSpace = True)
+    vertex_list: tuple[float, float, float] = cmds.xform(f"{shape}.vtx[*]", query = True, translation = True, worldSpace = True)
     vertex_positions = [Vector3(vertex_list[i], vertex_list[i+1], vertex_list[i+2]) for i in range(0, len(vertex_list), 3)]
 
     return vertex_positions
@@ -38,8 +60,9 @@ def map_vertices_to_curve(vertex_positions: list[Vector3], curve_shape: str) -> 
     cmds.delete(temp_info_node)
     return vertex_parameters
 
-def skin_mesh(bind_joints: list[str], geometry: str, name: str | None = None, dual_quaternion: bool = False) -> None:
+def skin_mesh(bind_joints: list[str], geometry: str, name: str | None = None, dual_quaternion: bool = False) -> str:
     if not name:
         name: str = f"{geometry}_SC"
-    cmds.skinCluster(bind_joints, geometry, skinMethod = 1 if dual_quaternion else 0, name=name)
-    pass
+    skin_cluster = cmds.skinCluster(bind_joints, geometry, skinMethod = 1 if dual_quaternion else 0, name=name)
+    return skin_cluster
+
